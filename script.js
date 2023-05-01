772992339999d5b33fc9e89802bc84d06eb71520
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-02 00:24:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function insert_Row() {
     //Write your code here
-  var table= documemt.getElementId("samleTable");
+  var table= documemt.getElementById("samleTable");
 
 	var newRow = table.insertRow(0);
 
 	var leftCell = newRow.insertCell(0);
-  var righttCell = newRow.insertCell(1);
+  var rightCell = newRow.insertCell(1);
 
 	leftCell.innerHTML= "New Cell1";
-	leftCell.innerHTML= "New Cell1";
+	rightCell.innerHTML= "New Cell2";
 }
