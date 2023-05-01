16a34d4afab25f6f23a27ab568330cd200fb7e1f
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-02 00:18:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  var table= documemt.getElementId("samleTable");
+
+	var newRow = table.insertRow(0);
+
+	var leftCell = newRow.insertCell(0);
+  var righttCell = newRow.insertCell(1);
+
+	leftCell.innerHTML= "New Cell1";
+	leftCell.innerHTML= "New Cell1";
 }
